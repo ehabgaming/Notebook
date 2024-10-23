6e08e2a4a5b6e09e3616c248ae8864faf89bc557
ehabgaming <46231767+ehabgaming@users.noreply.github.com>
2024-10-23 12:50:38 -0400

    Update midtermEHAB.cpp
    SHOULD BE DONE

--- midtermEHAB.cpp
+++ midtermEHAB.cpp
@@ -9,65 +9,70 @@
 #include <limits>
 
 //To do:
-//1. fix errors
-//2. fix ui
-//3. make the stats on one sport
-//4. put the function into the class
-//5. 
+//1. fix errors *DONE*
+//2. fix ui  *kinded done :/*
+//3. make the stats on one sport *DONE*
+//4. put the function into the class *DONE*
+//5. make the code more readble *i can read it just fine*
 
 using namespace std;
 
 // Player class to store the data from the file
 class Player {
 private:
-    string name;
+    string FirstName;
+    string LastName;
     string team;
-    double stat1;
-    double stat2;
-    double stat3;
-    double stat4;
-    double stat5;
+    double PPG;
+    double RPG;
+    double APG;
+    double BPG;
+    double SPG;
 
 public:
     // Constructor to initialize player data
-    Player(string n, string t, double s1, double s2, double s3, double s4, double s5) {
-        name = n;
+    Player(string fn,string ln, string t, double s1, double s2, double s3, double s4, double s5) {
+        FirstName = fn;
+        LastName = ln;
         team = t;
-        stat1 = s1;
-        stat2 = s2;
-        stat3 = s3;
-        stat4 = s4;
-        stat5 = s5;
+        PPG = s1;
+        RPG = s2;
+        APG = s3;
+        BPG = s4;
+        SPG = s5;
     }
 
     // Getters and Setters for player attributes
-    string getName() { return name; }
+    string getFirstName() { return FirstName; }
+    string getLastName() { return LastName; }
     string getTeam() { return team; }
-    double getStat1() { return stat1; }
-    double getStat2() { return stat2; }
-    double getStat3() { return stat3; }
-    double getStat4() { return stat4; }
-    double getStat5() { return stat5; }
+    double getPPG() { return PPG; }
+    double getRPG() { return RPG; }
+    double getAPG() { return APG; }
+    double getBPG() { return BPG; }
+    double getSPG() { return SPG; }
 
-    void setName(string n) { name = n; }
+    void setFirstName(string fn) { FirstName = fn; }
+    void setLastName(string ln) { FirstName = ln; }
     void setTeam(string t) { team = t; }
-    void setStat1(double s) { stat1 = s; }
-    void setStat2(double s) { stat2 = s; }
-    void setStat3(double s) { stat3 = s; }
-    void setStat4(double s) { stat4 = s; }
-    void setStat5(double s) { stat5 = s; }
+    void setPPG(double s) { PPG = s; }
+    void setRPG(double s) { RPG = s; }
+    void setAPG(double s) { APG = s; }
+    void setBPG(double s) { BPG = s; }
+    void setSPG(double s) { SPG = s; }
 
     // Function to print player data
     void printPlayer() {
-        cout << "Name: " << name << endl;
+        cout << "First Name: " << FirstName << endl;
+        cout << "Last Name: " << LastName << endl;
         cout << "Team: " << team << endl;
         // Set precision for floating-point numbers
         cout << fixed << setprecision(2);
-        cout << "PPG: " << stat1 << endl;
-        cout << "RPG: " << stat2 << endl;
-        cout << "APG: " << stat3 << endl;
-        cout << "BPG: " << stat4 << endl;
-        cout << "SPG: " << stat5 << endl;
+        cout << "PPG: " << PPG << endl;
+        cout << "RPG: " << RPG << endl;
+        cout << "APG: " << APG << endl;
+        cout << "BPG: " << BPG << endl;
+        cout << "SPG: " << SPG << endl;
         cout << defaultfloat; // Reset to default formatting
     }
 
@@ -83,12 +88,19 @@ public:
 void loadData(vector<Player>& players, string filename);
 void exportData(vector<Player>& players, string filename);
 
+void HoldandClearConsole() // a fuction to pause the screen and clear it after a key is pressed
+{
+    cout << "\nPress any key to continue....";
+    cin.get();
+    cin.get();
+}
+
 
 int main() {
     vector<Player> players;
-    Player p("ejan", "lakers", 10, 102, 30, 40, 50);
+    Player p("ejan","wood", "lakers", 10, 102, 30, 40, 50);
 
-    string inputFilename = "input.txt";   // Input file name
+    string inputFilename = "NBAPlayers.txt";   // Input file name
     string outputFilename = "Testout.txt"; // Output file name
 
     // Load data from file
@@ -97,6 +109,7 @@ int main() {
     int choice;
     do {
         // Display menu
+        system("cls");
         cout << "Menu Options:" << endl;
         cout << "1. Print out all users and statistics" << endl;
         cout << "2. Print out the statistics for a specific player" << endl;
@@ -117,7 +130,7 @@ int main() {
                 cin.ignore(numeric_limits< streamsize>::max(), '\n');        // than we discard the input for the max
                 cout << "Invaild input, you MUST enter a number\n";          
             }
-            else                // if the cin.fail = false than we let the user pass
+            else                                                             // if the cin.fail = false than we let the user pass
             {
                 break;
             }
@@ -129,28 +142,34 @@ int main() {
         switch (choice) {
         case 1:
             p.printAllPlayers(players);
+            HoldandClearConsole();
             break;
         case 2:
             p.printPlayerStats(players);
+            HoldandClearConsole();
             break;
         case 3:
             p.printTeamData(players);
+            HoldandClearConsole();
             break;
         case 4:
             p.updatePlayerData(players);
+            HoldandClearConsole();
             break;
         case 5:
             p.insertNewPlayer(players);
+            HoldandClearConsole();
             break;
         case 6:
             exportData(players, outputFilename);
+            HoldandClearConsole();
             break;
         case 7:
             cout << "Exiting program." << endl;
             break;
         default:
             cout << "Invalid choice. Please select again." << endl;
-            
+            cin.get();
         }
 
     } while (choice != 7);
@@ -165,12 +184,12 @@ void loadData(vector<Player>& players, string filename) {
         cout << "Error opening input file." << endl;
         return;
     }
-    string name, team;
+    string FirstName,LastName, team;
     double s1, s2, s3, s4, s5;
 
     // Read data for each player from the file
-    while (infile >> name >> team >> s1 >> s2 >> s3 >> s4 >> s5) {
-        Player p(name, team, s1, s2, s3, s4, s5);
+    while (infile >> FirstName >> LastName >> team >> s1 >> s2 >> s3 >> s4 >> s5) {
+        Player p(FirstName,LastName, team, s1, s2, s3, s4, s5);
         players.push_back(p);
     }
     infile.close();
@@ -187,17 +206,21 @@ void Player::printAllPlayers(vector<Player>& players) {
 
 // Function to print statistics for a specific player
 void Player::printPlayerStats(vector<Player>& players) {
-    string name;
-    cout << "Enter the player's name: ";
-    cin >> name;
+    string FirstName, LastName;
+    cout << "Enter the player's First Name: ";
+    cin >> FirstName;
+    cout << "Enter the player's Last Name: ";
+    cin >> LastName;
     bool found = false;
+
     // Search for the player by name
     for (size_t i = 0; i < players.size(); ++i) {
-        if (players[i].getName() == name) {
+        if (players[i].getFirstName() == FirstName || players[i].getFirstName() == LastName) {
             players[i].printPlayer();
             found = true;
             break;
         }
+        
     }
     if (!found) {
         cout << "Player not found." << endl;
@@ -225,13 +248,19 @@ void Player::printTeamData(vector<Player>& players) {
 
 // Function to update data for a specific player
 void Player::updatePlayerData(vector<Player>& players) {
-    string name;
-    cout << "Enter the player's name to update: ";
-    cin >> name;
+    string FirstName, LastName;
+    cout << "Enter the player's name to update\n";
+
+    cout << "Enter the player's First Name: ";
+    cin >> FirstName;
+
+    cout << "Enter the player's Last Name: ";
+    cin >> LastName;
+
     bool found = false;
     // Search for the player by name
     for (size_t i = 0; i < players.size(); ++i) {
-        if (players[i].getName() == name) {
+        if (players[i].getFirstName() == FirstName || players[i].getFirstName() == LastName) {
             string t;
             double s1, s2, s3, s4, s5;
             cout << "Enter new Team: ";
@@ -248,11 +277,11 @@ void Player::updatePlayerData(vector<Player>& players) {
             cin >> s5;
             // Update player's statistics
             players[i].setTeam(t);
-            players[i].setStat1(s1);
-            players[i].setStat2(s2);
-            players[i].setStat3(s3);
-            players[i].setStat4(s4);
-            players[i].setStat5(s5);
+            players[i].setPPG(s1);
+            players[i].setRPG(s2);
+            players[i].setAPG(s3);
+            players[i].setBPG(s4);
+            players[i].setSPG(s5);
             cout << "Player data updated." << endl;
             found = true;
             break;
@@ -265,10 +294,12 @@ void Player::updatePlayerData(vector<Player>& players) {
 
 // Function to insert a new player and their statistics
 void Player::insertNewPlayer(vector<Player>& players) {
-    string name, team;
+    string FirstName,LastName, team;
     double s1, s2, s3, s4, s5;
-    cout << "Enter player's name: ";
-    cin >> name;
+    cout << "Enter player's First name: ";
+    cin >> FirstName;
+    cout << "Enter player's Last name: ";
+    cin >> LastName;
     cout << "Enter player's team: ";
     cin >> team;
     cout << "Enter PPG: ";
@@ -282,7 +313,7 @@ void Player::insertNewPlayer(vector<Player>& players) {
     cout << "Enter SPG: ";
     cin >> s5;
     // Create a new player and add to the list
-    Player p(name, team, s1, s2, s3, s4, s5);
+    Player p(FirstName,LastName, team, s1, s2, s3, s4, s5);
     players.push_back(p);
     cout << "New player added." << endl;
 }
@@ -296,13 +327,14 @@ void exportData(vector<Player>& players, string filename) {
     }
     // Write each player's data to the file
     for (size_t i = 0; i < players.size(); ++i) {
-        outfile << players[i].getName() << " "
+        outfile << players[i].getFirstName() << " "
+            << players[i].getLastName() << " "
             << players[i].getTeam() << " "
-            << players[i].getStat1() << " "
-            << players[i].getStat2() << " "
-            << players[i].getStat3() << " "
-            << players[i].getStat4() << " "
-            << players[i].getStat5() << endl;
+            << players[i].getPPG() << " "
+            << players[i].getRPG() << " "
+            << players[i].getAPG() << " "
+            << players[i].getBPG() << " "
+            << players[i].getSPG() << endl;
     }
     outfile.close();
     cout << "Data exported to " << filename << endl;
